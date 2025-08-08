27d93da5a6a650a23626a50f0bb8a5cc245015cb
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-08-08 21:12:41 +0700

    todos-list

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-const express = require('express');
-const cors = require('cors');
-
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const port = 3001;
+const cors = require("cors");
+const port = process.env.PORT ;
+const { router: todoRoutes, todos } = require("./routes/todo.js");
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-const todoRouter = require('./routes/todo.js');
+app.use("/todos", todoRoutes);
 
-app.use("/todos", todoRouter);
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/todos-data", (req, res) => {
+  res.json(todos);
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact'); 
+app.get("/todos-page", (req, res) => {
+  res.render("todos-page", { 
+    todos: todos,
+    activePage: 'todos'
+  });
 });
 
-//midleware
+app.get("/", (req, res) => {
+  res.render("index", {
+    activePage: 'home'
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    activePage: 'contact'
+  });
+});
+
 app.use((req, res) => {
-    res.status(404).send('Page not found 404');
+  res.status(404).send("404 - Page Not Found");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
